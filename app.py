624596769e8a6cb1e7714c5bb4e10b5b524b9548
sh-mukherjee <71715720+sh-mukherjee@users.noreply.github.com>
2024-07-05 23:38:44 +0800

    Shiny Express app

---
+++ app.py
@@ -0,0 +1,36 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shiny import reactive
+from shinywidgets import render_plotly
+
+from ETF_sector_data_prep import sector_df, countries
+ui.h1("MSCI ETF Sector Weights")
+ui.p("Source: Yahoo! Finance")
+
+with ui.sidebar():
+    ui.input_selectize('Country', 'Select Country', countries, multiple=True)
+    
+
+@reactive.calc
+def filter_data():
+    mask = sector_df.Country.isin(input.Country())
+    return sector_df[mask]
+
+@render_plotly
+def plot1():
+    return px.bar(filter_data(), 
+                  x="Weight(%)", 
+                  y="Sector_Name", 
+                  color="Country", hover_data=['Country','Sector_Name','Weight(%)'],
+             color_discrete_map={
+                "Japan": "red",
+                "Taiwan": "#00CC96",
+                "South Korea": "blue",
+                "Hong Kong": "#FBE426",
+                "Singapore": "magenta",
+             "Australia": "goldenrod",
+             "Malaysia": "green",
+             "Thailand": "darkblue"},
+             template="ggplot2",   
+             barmode = 'group',
+             height = 800)
